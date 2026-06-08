8434dc048e2a8a554cca6328369105b96dfda539
nmichaud <nmichaud@f4r3s3.paris.42.school>
2026-06-08 13:33:15 +0200

    draw sprites modularisee

--- src/sprites_src/draw_sprites.c
+++ src/sprites_src/draw_sprites.c
@@ -12,25 +12,96 @@
 
 #include "../../inc/cube_3d.h"
 
-int	draw_sprites(t_data *data, t_player *player)
+typedef struct s_sprite_type
 {
-	int		*order;
-	int		i;
-	int		x;
-	int		y;
 	int		color;
-	double	sprite_x;
-	double	sprite_y;
+	double	x;
+	double	y;
 	double	inv_det;
 	double	transform_x;
 	double	transform_y;
-	int		sprite_screen_x;
-	int		sprite_h;
-	int		sprite_w;
+	int		screen_x;
+	int		h;
+	int		w;
 	int		draw_start_y;
 	int		draw_start_x;
 	int		draw_end_y;
 	int		draw_end_x;
+}	t_sprite_type;
+
+void	compute_sprite_transformation(t_data *data, t_player *player, int sprite_id, t_sprite_type *sprite)
+{
+	sprite->x = data->sprites[sprite_id].x - player->pos_x;
+	sprite->y = data->sprites[sprite_id].y - player->pos_y;
+	sprite->inv_det = 1.0 / (player->plane_x * player->dir_y - player->dir_x * player->plane_y);
+	sprite->transform_x = sprite->inv_det * (player->dir_y * sprite->x - player->dir_x * sprite->y);
+	sprite->transform_y = sprite->inv_det * (-player->plane_y * sprite->x + player->plane_x * sprite->y);
+}
+
+void	compute_sprite_bounds(t_data *data, t_sprite_type *sprite)
+{
+	sprite->screen_x = (int)((data->screen.w / 2) * (1 + sprite->transform_x / sprite->transform_y));
+	sprite->h = abs((int)(data->screen.h / sprite->transform_y));
+	sprite->draw_start_y = - sprite->h / 2 + data->screen.h / 2;
+	if (sprite->draw_start_y < 0)
+		sprite->draw_start_y = 0;
+	sprite->draw_end_y = sprite->h / 2 + data->screen.h / 2;
+	if (sprite->draw_end_y >= data->screen.h)
+		sprite->draw_end_y = data->screen.h - 1;
+	sprite->w = abs((int)(data->screen.h / sprite->transform_y)); // w au lieu de h ?
+	sprite->draw_start_x = - sprite->w / 2 + sprite->screen_x;
+	if (sprite->draw_start_x < 0)
+		sprite->draw_start_x = 0;
+	sprite->draw_end_x = sprite->w / 2 + sprite->screen_x;
+	if (sprite->draw_end_x >= data->screen.w)
+		sprite->draw_end_x = data->screen.w - 1;
+}
+
+void	render_sprite(t_data *data, int sprite_id, t_sprite_type *sprite)
+{
+	int	x;
+	int	y;
+
+	x = sprite->draw_start_x;
+	while (x < sprite->draw_end_x)
+	{
+		int	tex_x = (int)(256 * (x - (- sprite->w / 2 + sprite->screen_x)) * data->sprites[sprite_id].texture->w / sprite->w) / 256;
+		if (sprite->transform_y > 0 && x > 0 && x < data->screen.w && sprite->transform_y < data->ray[x].perp_wall_dist)
+		{
+			y = sprite->draw_start_y;
+			while (y < sprite->draw_end_y)
+			{
+				int d = y * 256 - data->screen.h * 128 + sprite->h * 128;
+				int	tex_y = (d * data->sprites[sprite_id].texture->h / sprite->h) / 256;
+				if (tex_x < data->sprites[sprite_id].texture->w && tex_x >= 0
+					&& tex_y < data->sprites[sprite_id].texture->h && tex_y >= 0)
+				{
+					sprite->color = get_pixel(data->sprites[sprite_id].texture, tex_x, tex_y);
+					if (sprite->color != 0)
+						my_mlx_pixel_put(&data->screen, x, y, sprite->color);
+				}
+				y++;
+			}
+		}
+		x++;
+	}
+}
+
+void	draw_single_sprite(t_data *data, t_player *player, int sprite_id)
+{
+	t_sprite_type	sprite;
+
+	compute_sprite_transformation(data, player, sprite_id, &sprite);
+	if (sprite.transform_y <= 0)
+		return ;
+	compute_sprite_bounds(data, &sprite);
+	render_sprite(data, sprite_id, &sprite);
+}
+
+int	draw_sprites(t_data *data, t_player *player)
+{
+	int		*order;
+	int		i;
 
 	order = sort_sprites(data, data->nb_sprites);
 	if (!order)
@@ -38,54 +109,7 @@ int	draw_sprites(t_data *data, t_player *player)
 	i = 0;
 	while (i < data->nb_sprites)
 	{
-		sprite_x = data->sprites[order[i]].x - player->pos_x;
-		sprite_y = data->sprites[order[i]].y - player->pos_y;
-		inv_det = 1.0 / (player->plane_x * player->dir_y - player->dir_x * player->plane_y);
-		transform_x = inv_det * (player->dir_y * sprite_x - player->dir_x * sprite_y);
-		transform_y = inv_det * (-player->plane_y * sprite_x + player->plane_x * sprite_y);
-		if (transform_y <= 0)
-		{
-			i++;
-			continue ;
-		}
-		sprite_screen_x = (int)((data->screen.w / 2) * (1 + transform_x / transform_y));
-		sprite_h = abs((int)(data->screen.h / transform_y));
-		draw_start_y = - sprite_h / 2 + data->screen.h / 2;
-		if (draw_start_y < 0)
-			draw_start_y = 0;
-		draw_end_y = sprite_h / 2 + data->screen.h / 2;
-		if (draw_end_y >= data->screen.h)
-			draw_end_y = data->screen.h - 1;
-		sprite_w = abs((int)(data->screen.h / transform_y)); // w au lieu de h ?
-		draw_start_x = - sprite_w / 2 + sprite_screen_x;
-		if (draw_start_x < 0)
-			draw_start_x = 0;
-		draw_end_x = sprite_w / 2 + sprite_screen_x;
-		if (draw_end_x >= data->screen.w)
-			draw_end_x = data->screen.w - 1;
-		x = draw_start_x;
-		while (x < draw_end_x)
-		{
-			int	tex_x = (int)(256 * (x - (- sprite_w / 2 + sprite_screen_x)) * data->sprites[order[i]].texture->w / sprite_w) / 256;
-			if (transform_y > 0 && x > 0 && x < data->screen.w && transform_y < data->ray[x].perp_wall_dist)
-			{
-				y = draw_start_y;
-				while (y < draw_end_y)
-				{
-					int d = y * 256 - data->screen.h * 128 + sprite_h * 128;
-					int	tex_y = (d * data->sprites[order[i]].texture->h / sprite_h) / 256;
-					if (tex_x < data->sprites[order[i]].texture->w && tex_x >= 0
-						&& tex_y < data->sprites[order[i]].texture->h && tex_y >= 0)
-					{
-						color = get_pixel(data->sprites[order[i]].texture, tex_x, tex_y);
-						if (color != 0)
-							my_mlx_pixel_put(&data->screen, x, y, color);
-					}
-					y++;
-				}
-			}
-			x++;
-		}
+		draw_single_sprite(data, player, order[i]);
 		i++;
 	}
 	free(order);
