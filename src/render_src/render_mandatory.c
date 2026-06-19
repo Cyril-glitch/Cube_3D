0933292780b4928dd9dfe67355d80175b2b1cbcf
nmichaud <nmichaud@f1r7s17.paris.42.school>
2026-06-19 16:54:15 +0200

    mandatory part separee

---
+++ src/render_src/render_mandatory.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render_mandatory.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmichaud <nmichaud@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/19 16:35:20 by nmichaud          #+#    #+#             */
+/*   Updated: 2026/06/19 16:35:20 by nmichaud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/cube_3d.h"
+
+void    ft_wall_draw(t_ray *ray, t_data *data)
+{
+	int x;
+	int y;
+	int	tex_x;
+	int	tex_y;
+	int color;
+
+	x = 0;
+	while(x < data->win_size.x)
+	{
+        y = 0;
+		while (y < ray[x].draw_start)
+		{
+			color = data->map.ceiling_color;
+			my_mlx_pixel_put(&data->screen, x, y, color);
+			y++;
+		}
+		y = ray[x].draw_start;
+		while(y < ray[x].draw_end)
+		{
+			tex_x = ray[x].tex_x;
+			tex_y = (int)ray[x].tex_pos & (data->textures[ray[x].tex_num].h - 1);
+			ray[x].tex_pos += ray[x].step;
+			color = get_pixel(&data->textures[ray[x].tex_num], tex_x, tex_y);
+			if (ray[x].side == 1)
+				color = (color >> 1) & 8355711;
+			my_mlx_pixel_put(&data->screen, x, y, color);
+			y++;
+		}
+		while (y < data->win_size.y)
+		{
+			color = data->map.floor_color;
+			my_mlx_pixel_put(&data->screen, x, y, color);
+			y++;
+		}
+		x++;
+	}
+}
+
+void	ft_render_map(t_data *data, t_mini_map *map)
+{
+	int	x;
+	int	y;
+	int	color;
+
+	x = 10;
+	while (x < map->size + 10)
+	{
+		y = 10;
+		while (y < map->size + 10)
+		{
+			color = get_pixel(&map->image, x - 10, y - 10);
+			/*ft_putnbr_base((long)color, "0123456789ABCDEF");
+			write(1, "\n", 1);*/
+			my_mlx_pixel_put(&data->screen, x, y, color);
+			y++;
+		}
+		x++;
+	}
+}
+
+void    ft_render_draw(t_ray *ray, t_data *data)
+{
+	//double t0 = get_time(data->start);
+	/*if (!data->player.hp.health)
+	{
+		ft_render_death(data, data->over);
+		return;
+	}*/
+	(void)ray;
+	//double t0 = get_time(data->start);
+	//ft_render_fc(data, &data->bgrd, &data->bgrd.floor, &data->bgrd.ceiling);
+	//printf("floor / ceiling: %.3f ms\n", get_time(data->start) - t0);
+	//t0 = get_time(data->start);
+	ft_wall_draw(ray, data);
+	//printf("wall_draw: %.3f ms\n", get_time(data->start) - t0);
+	//t0 = get_time(data->start);
+	//if (!draw_sprites(data, &data->player))
+	//	return ;
+	//printf("sprites: %.3f ms\n", get_time(data->start) - t0);
+	//ft_render_ath(data);
+	//printf("sprites: %.3f ms\n", get_time(data->start) - t0);
+	/*if (data->mini_map.exist)
+	{
+		ft_memset(data->mini_map.image.addr, 0, data->mini_map.size * data->mini_map.size * 4);
+		draw_map_img(data, &data->mini_map, data->player);
+		ft_render_map(data, &data->mini_map);
+	}*/
+}
---
+++ src/update/update_mandatory.c
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   update_mandatory.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmichaud <nmichaud@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/19 16:45:04 by nmichaud          #+#    #+#             */
+/*   Updated: 2026/06/19 16:45:05 by nmichaud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/cube_3d.h"
+
+void	update_time(t_data *data)
+{
+	data->old_time = data->time;
+	data->time = get_time(data->start);
+	data->frame_time = (data->time - data->old_time) / 1000.0;
+	data->player.rot_speed = data->frame_time * 2.5;
+	data->player.move_speed = data->frame_time * 3.0;
+}
+
+void	handle_movements(t_data *data)
+{
+	if (data->keys.w)
+		move_up(data);
+	if (data->keys.a)
+		move_left(data);
+	if (data->keys.s)
+		move_down(data);	
+	if (data->keys.d)
+		move_right(data);
+}
+
+void	handle_camera(t_data *data)
+{
+	if (data->keys.left)
+		ft_rot_left(data);
+	if (data->keys.right)
+		ft_rot_right(data);
+}
+
+void	update_doors(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->nb_doors)
+	{
+		if (data->doors[i].opening)
+		{
+			data->doors[i].open += data->frame_time * 1.5f;
+			if (data->doors[i].open >= 1.0f)
+			{
+				data->doors[i].open = 1.0f;
+				data->doors[i].opening = false;
+			}
+		}
+		if (data->doors[i].closing)
+		{
+			data->doors[i].open -= data->frame_time * 1.5f;
+			if (data->doors[i].open <= 0.0f)
+			{
+				data->doors[i].open = 0.0f;
+				data->doors[i].closing = false;
+			}
+		}
+		i++;
+	}
+}
+
+t_sprite	*get_treasure_sprite(t_sprite *sprites, int count, int x, int y)
+{
+	int	i;
+
+	i = 0;
+	while (i < count)
+	{
+		if ((int)sprites[i].x == x && (int)sprites[i].y == y)
+			return (&sprites[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+void	update_treasures(t_data *data)
+{
+	t_sprite	*sprite;
+
+	sprite = NULL;
+	if (get_map_tile(data->map.grid, data->map.height, (int)data->player.pos_x, (int)data->player.pos_y) == SPRITE_T - '0')
+		sprite = get_treasure_sprite(data->sprites, data->t_sprites->number, (int)data->player.pos_x, (int)data->player.pos_y);
+	if (sprite)
+		sprite->consumed = true;
+}
+
+void	update_monsters(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->m_sprites->number)
+	{
+		if (ft_monster_arrived(&data->monsters[i]))
+			ft_update_monster_path(data, &data->monsters[i], i);
+		ft_bot_move(data, i);
+		i++;
+	}
+}
+
+void	update(t_data *data)
+{
+	update_time(data);
+	handle_movements(data);
+	handle_camera(data);
+}
+
